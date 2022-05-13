d2e56fe837772c9458123972d3bdcdc17e682232
mat <mat@mats-iMac.local>
2022-05-13 20:37:27 -0300

    Update a countrie

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ server.post('countries', (req, res) =>{
     return res.json(countries)
 })
 
+// Update a countrie
+server.put('/countries/:index', (req, res) =>{
+    const = { index } = req.params
+    const = { name } = req.body
 
+    countries[index] = name
+    return res.json(countries)
+})
 
 server.listen(3001)
